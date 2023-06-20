77bb2435abf626a6f9d42a97a729b61513b29489
Rusty-Helix <a0900492616@gmail.com>
2023-06-20 11:34:48 +0800

    before submit logic

--- core/views.py
+++ core/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Item
 
+from .forms import SignupForm
+
 def index(request):
     context = {}
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -18,9 +20,21 @@ def index(request):
 
 def contact(request):
     context = {}
-    return render(request, 'core/contact.html')
+    return render(request, 'core/contact.html', context)
+
+def signup(request):
+    form = SignupForm()
+    
+    return render(request, 'core/signup.html', {
+            'form': form
+        })
 
 def detail(request, pk): # card details
     item = get_object_or_404(Item, pk=pk)
-    context = {'item': item}
-    return render(request, 'item/detail.html', context)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)
+    # print(related_items)
+
+
+    context = {'item': item,
+               'related_items': related_items}
+    return render(request, 'core/detail.html', context)
--- puddle/urls.py
+++ puddle/urls.py
@@ -1,13 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from core.views import index, contact
+from core.views import detail
 # from core import views.index
 
+# app_name = 'item' # create namespace
+
 urlpatterns = [
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    path('items/<int:pk>/', detail, name='detail')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
